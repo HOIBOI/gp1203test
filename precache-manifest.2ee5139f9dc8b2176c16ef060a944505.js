5ce943801a7b597b49b765da1dbe18bde52912d8
HOIBOI <ahabdmer@gmail.com>
2025-03-27 10:16:14 -0700

    Updates

---
+++ precache-manifest.2ee5139f9dc8b2176c16ef060a944505.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761efb46a1a2c6dbd53d9b5d2d8d60e4",
+    "url": "/gp1203test/index.html"
+  },
+  {
+    "revision": "ae89f127c14dc0ca2187",
+    "url": "/gp1203test/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "91532fe74c9ded204142",
+    "url": "/gp1203test/static/js/2.a7f37d0f.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/gp1203test/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "ae89f127c14dc0ca2187",
+    "url": "/gp1203test/static/js/main.b46b5bb6.chunk.js"
+  },
+  {
+    "revision": "9057edcbb1b22ec3c116",
+    "url": "/gp1203test/static/js/runtime~main.37803e98.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/gp1203test/static/media/logo.06e73328.svg"
+  }
+]);
